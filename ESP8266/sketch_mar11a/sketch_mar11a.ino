88290a6d3bd2e622d7a1bf8b6f14a6a3c751e693
louis-chevallier <louis.chevallier@gmail.com>
2022-05-27 00:03:24 +0200

    bbox

--- ESP8266/sketch_mar11a/sketch_mar11a.ino
+++ ESP8266/sketch_mar11a/sketch_mar11a.ino
@@ -4,7 +4,7 @@
 
 ESP8266WebServer server(80);
 
-const char* ssid = "CHEVALLIER_BORDEAU_F"; //Enter Wi-Fi SSID
+const char* ssid = "CHEVALLIER_BORDEAU"; //Enter Wi-Fi SSID
 const char* password =  "9697abcdea"; //Enter Wi-Fi Password
 long count = 0;
 int ledv = 1>2;
@@ -15,7 +15,7 @@ long PINOUT=15;
 
 void handle_index_main() {
   start = count;
-  Serial.print("main");
+  Serial.print("handle_index_main");
   //Print Hello at opening homepage
   String message("count =");
   message += String(count);
@@ -30,6 +30,7 @@ void handle_index_main() {
   delay(2000);
   digitalWrite(2, HIGH);   // Turn the LED on (Note that LOW is the voltage level
   digitalWrite(PINOUT, LOW); 
+  Serial.print("end");
 }
 
  
@@ -59,7 +60,8 @@ void handle_index_main() {
                       button.innerHTML = accueil;
                       button.disabled = false;
                   };
-      const url = "http://78.207.134.29:8083/main";
+      //const url = "http://78.207.134.29:8083/main";
+      const url = "http://192.168.1.95/main";
       function getCookie(cname) {
         let name = cname + "=";
         let decodedCookie = decodeURIComponent(document.cookie);
@@ -78,6 +80,7 @@ void handle_index_main() {
       code =getCookie("eportiercode");
       console.log(code);
       function ouvre(cde) {
+                console.log(cde);
                 document.cookie = "eportiercode=" + cde;
                 cookies = document.cookies;
                 console.log(cookies);
@@ -87,6 +90,7 @@ void handle_index_main() {
                 button.innerHTML = "le verrou va s'ouvrir ...";
                 setTimeout(() => {
                         button.disabled = true;
+                        console.log(url+code)
                         window.fetch(url+cde, { mode: 'no-cors'}).then((result) => {
                                 //console.log(result);
                                 if (result.ok) {                                
@@ -109,14 +113,14 @@ void handle_index_main() {
         // Create a button element
           const nbutton = document.createElement('button');
           nbutton.innerText = txt;
-          //console.log(nbutton);
+          console.log(nbutton);
           nbutton.style.position = "absolute";
           nbutton.style.left = x + 'px';
           nbutton.style.top = y + 'px';
           nbutton.style.fontSize = "120px";
           nbutton.style.backgroundColor = 'White';
           nbutton.addEventListener('click', function() {
-              //console.log( this );
+              console.log( this );
               this.style.backgroundColor = 'Red';
               this.disabled = true;
               clicked.push(this);
@@ -142,7 +146,7 @@ void handle_index_main() {
       MH = 200;
       //console.log("create buttons");
       for (i = 1; i < 10; i++) {
-        //console.log(i);
+        console.log(i);
         addbutton(i, ((i-1) % 3) * W + ML, (~~((i-1) / 3) * H) + MH); 
       }                        
     </script>
@@ -156,6 +160,7 @@ void handle_index() {
   //page.replace(""", "\"");
  
   server.send(505, "text/html", page.c_str());
+  Serial.print("end");
 }
 
 void setup() {
@@ -182,7 +187,7 @@ void setup() {
   server.on("/main96713", handle_index_main); //Handle Index page
   
   server.begin(); //Start the server
-  Serial.println("Server listening");
+  Serial.println("setup");
   pinMode(2, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
   digitalWrite(2, HIGH);  
 
@@ -192,6 +197,7 @@ void setup() {
   
   pinMode(PINOUT, OUTPUT);
   digitalWrite(PINOUT, LOW);  
+  Serial.println("Server listening");
 }
 
 void loop() {
