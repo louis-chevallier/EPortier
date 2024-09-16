afaa23df4bdc77a2f7d19e8a08594af8e13d8b12
jitdeep chehara <jitdeep.chehara@gmail.com>
2024-09-16 22:38:32 +0200

    test-swap

--- ESP8266/test/code.js
+++ ESP8266/test/code.js
@@ -1,7 +1,8 @@
 count = "a";
 const accueil = "Tapez le code";
 const button = document.getElementById("ouvrir");
-const swap = document.getElementById("swap");
+const statut = document.getElementById("statut");
+const swap_button = document.getElementById("swap");
 
 /*
 const ws = new WebSocket('ws://IPADDRESS:PORT/ws')
@@ -16,30 +17,6 @@ ws.onmessage = (message) => {
 }
 */
 
-function statut() {
-    murl = "statut_porte";
-    //console.log("fetching");
-    count = count + "a";
-    //document.getElementById("statut").innerHTML = "fetching";
-    fetch(murl).then(function(response) {
-        //console.log("reponse");
-        d = response.json();
-        //console.log(d);
-        //console.log(d["porte"]);
-        return d;
-    }).then(function(data) {
-        //button.innerHTML = count;
-        //console.log("data");
-        //console.log(data);
-        document.getElementById("statut").innerHTML = "La porte est " + data["porte"];
-        setTimeout(statut, 1000);
-    }).catch(function(ee) {
-        console.log("Booo");
-        //document.getElementById("statut").innerHTML = ee;
-        
-    });
-}
-const myTimeout = setTimeout(statut, 1000);
 
 cookies = document.cookies;
 console.log(cookies);
@@ -52,6 +29,7 @@ reset = function(){
 //const url = "http://78.207.134.29:8083/main";
 //const url = "http://78.207.134.29:8083/main";
 const url = "WURL";
+const root_url = "ROOT_URL";
 //const url = "http://192.168.1.95/main";
 
 function getCookie(cname) {
@@ -71,99 +49,57 @@ function getCookie(cname) {
 }
 code =getCookie("eportiercode");
 console.log(code);
-function ouvre(cde) {
-    console.log(cde);
-    document.cookie = "eportiercode=" + cde + ";SameSite=Strict";
-    cookies = document.cookies;
-    console.log(cookies);
-    cookies = document.cookie;
-    console.log(cookies);
-    button.style.fontSize="30px";
-    button.innerHTML = "le verrou va s'ouvrir ...";
-    setTimeout(() => {
-        button.disabled = true;
-        console.log(url+code)
-        window.fetch(url+cde, { mode: 'no-cors'}).then((result) => {
-            console.log(result);
-            if (result.ok) {                                
-                button.innerHTML = "Système contacté,<br>déverrouillage pendant 2 secondes ...";
-            } else {
-                button.innerHTML = "Système contacté .. bad luck!";
-            }
-            setTimeout(reset, 2000);
-        }).catch((e) => {
-            button.innerHTML = "Pas moyen de contacter le système!.. fetching " + url + cde;
-            setTimeout(reset, 2000);
-        })}, 
-               1000);
-}
 
 function debug() {
-    window.fetch(url, { mode: 'no-cors'}).then((result) => {
-        let delay_msec = Math.floor((Math.random() * (6 - 2 + 1) + 1) * 1000);
-        setTimeout(debug, delay_msec);
-    });
+    console.log('url=', url);
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "./main");
+    xhr.send();
+    xhr.responseType = "json";
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            try {
+                response = xhr.response;
+                console.log('status', response.status);
+                console.log('swapped', response.swapped);
+                console.log('buf_serial', response.buf_serial);
+                var ss = "swapped : " + response.swapped + "\n";
+                ss += "buf_serial  : " + response.buf_serial + "\n";
+                statut.innerHTML =  ss;
+            } catch (error) {
+                console.log("error ", error);
+            }
+            const delay_msec = Math.floor((Math.random() * 4) * 1000);
+            console.log(delay_msec);
+            setTimeout(debug, delay_msec);
+        }
+    }
 }
 
-function swap_func(cde) {
-    murl = "swap";
-    fetch(murl).then(function(response) {
-        d = response.json();
-        return d;
-    }).then(function(data) {
-        document.getElementById("statut").innerHTML = "La porte est " + data["porte"];
-        setTimeout(statut, 1000);
-    }).catch(function(ee) {
-        console.log("Booo");
-        //document.getElementById("statut").innerHTML = ee;
-        
-    });    
+function swap_func() {
+    console.log('url=', root_url);
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "./swap");
+    xhr.send();
+    xhr.responseType = "json";
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            response = xhr.response;
+            console.log('status', response.status);
+        }
+    }
 }
 
-button.addEventListener('click', function() { ouvre(code); });
-swap.addEventListener('click', function() { swap_func(); });
+
+button.addEventListener('click', function() { debug();  });
+swap_button.addEventListener('click', function() { swap_func();  });
+
 reset();
 buttons = [];
 clicked = [];
-function addbutton(txt, x, y) {
-    // Create a button element
-    const nbutton = document.createElement('button');
-    nbutton.innerText = txt;
-    console.log(nbutton);
-    nbutton.style.position = "absolute";
-    nbutton.style.left = x + 'px';
-    nbutton.style.top = y + 'px';
-    nbutton.style.fontSize = "120px";
-    nbutton.style.backgroundColor = 'White';
-    nbutton.addEventListener('click', function() {
-        console.log( this );
-        this.style.backgroundColor = 'Red';
-        this.disabled = true;
-        clicked.push(this);
-        if (clicked.length == 5) {
-            code = "";
-            for (i=0; i < clicked.length; i++) {
-                b = clicked[i];
-                code += b.innerText;
-                b.style.backgroundColor = 'White';
-                b.disabled = false;
-            };
-            ouvre(code);
-            //console.log(code);
-            clicked = [];
-        }
-    });
-    buttons.push(nbutton);
-    document.body.appendChild(nbutton);
-};
 W=30*4;
 H=75*2;
 ML=  100;
 MH = 230;
-//console.log("create buttons");
-for (i = 1; i < 10; i++) {
-    console.log(i);
-    addbutton(i, ((i-1) % 3) * W + ML, (~~((i-1) / 3) * H) + MH); 
-}                        
 
 debug();
--- ESP8266/test/test.ino
+++ ESP8266/test/test.ino
@@ -11,7 +11,8 @@ long seko = millis();
 #define EKOX(x) Serial.println(S + __FILE__ + ":" + String(__LINE__) + ": [" + String(millis()-seko) + "ms] " + #x + "=" + String(x) + "."); seko=millis()
 #define EKO()   Serial.println(S + __FILE__ + ":" + String(__LINE__) + ": [" + String(millis()-seko) + "ms]"); seko=millis()
 
-const String WURL = String("http://") + String(IPADDRESS) + ":" + String(PORT) + "/main";
+const String ROOT_URL = String("http://") + String(IPADDRESS) + ":" + String(PORT);
+const String WURL = ROOT_URL + "/main";
   
 // chez nous
 const char* ssid = "CHEVALLIER_BORDEAU"; //Enter Wi-Fi SSID
@@ -22,13 +23,34 @@ String jscode((const char*)bin2c_code_js);
 #include "page.h"
 String page((const char*)bin2c_page_html);
 
+String buf_serial;
+bool swapped(false);
+
+void swap() {
+  delay(500);
+  Serial.swap();
+  delay(500);  
+  //Serial.begin(115200); //Begin Serial at 115200 Baud
+  swapped = !swapped;
+  EKOX(swapped);
+}
+
+#define G(x) (S + "\"" + String(x) + "\"")
+
+
 void setup() {
   
+
   pinMode(A0,INPUT);
 
   Serial.begin(115200); //Begin Serial at 115200 Baud
   Serial.print("starting");
-  delay(10);
+  delay(500);
+  String sss = "test.ino:69: [450ms]";
+  for (int i = 0; i < sss.length(); i++) {
+    EKOX(int(sss[i]));
+  }
+  EKOX(sss);
 
   EKO();
   //pinMode(2, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
@@ -50,17 +72,31 @@ void setup() {
   String ipaddr = WiFi.localIP().toString(); 
   EKOX(ipaddr); 
   server.on("/main", HTTP_GET, [](){
+    //EKO();
+    String npage("{");
+    npage += S + "\"swapped\" : " + G(swapped) ;
+    npage += S + "," + G("status") + " : " + G("ok");
+    npage += S + "," + G("buf_serial") + " : " + G(buf_serial.length());
+    npage += " }";
+    //EKOX(npage);
+    server.send(200, "text/json", npage.c_str());
+  });
+  server.on("/swap", HTTP_GET, [](){
     EKO();
+    swap();
+    String npage("{");
+    npage += S + G("status") + " : " + G("ok");
+    npage += " }";
+    server.send(200, "text/json", npage.c_str());
   });
   server.on("/", HTTP_GET, [](){
     EKO();
-      String npage(page);
-      npage.replace("PORT", String(PORT));
-      npage.replace("IPADDRESS", String(IPADDRESS));
-      npage.replace("WURL", WURL);
-      
-      server.send(505, "text/html", npage.c_str());
-      
+    String npage(page);
+    npage.replace("PORT", String(PORT));
+    npage.replace("IPADDRESS", String(IPADDRESS));
+    npage.replace("WURL", WURL);
+    npage.replace("ROOT_URL", ROOT_URL);    
+    server.send(505, "text/html", npage.c_str());
 
   });
   server.begin();
@@ -69,11 +105,31 @@ void setup() {
 
 int last = 0;
 
+
+
 void loop() {
   server.handleClient();
   auto now = millis();
+  if (swapped) {
+    if (Serial.available()) {
+      char c = Serial.read();
+      buf_serial += String(c);
+    }
+  } else {
+    if (buf_serial.length() > 0) {
+      EKOX(buf_serial.length());
+      for (int i = 0; i < buf_serial.length(); i++) {
+        EKOX(int(buf_serial[i]));
+      }
+
+      
+      buf_serial = "";
+    }
+  }
+
+  
   if (now > last + 1000) {
     last = now;
-    EKO();
+    //EKO();
   }
 }
