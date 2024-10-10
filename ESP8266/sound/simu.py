43a245c3d064137501af332a910cd59d3dfe2797
louis <louis.chevallier@gmail.com>
2024-10-10 23:24:02 +0200

    peak

--- ESP8266/sound/simu.py
+++ ESP8266/sound/simu.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 import matplotlib.pyplot as plt
 import math
 import PySpice.Logging.Logging as Logging
@@ -24,7 +25,7 @@ scale_factor = 1. #10000
 data = data / scale_factor
 EKOX(np.amin(data))
 EKOX(np.amax(data))
-duration = 3. # sec
+duration = 3.  # sec
 t = np.arange(0, duration, 1./samplerate)
 EKOX(t.shape)
 EKOX(data.shape)
@@ -52,33 +53,44 @@ if False :
     circuit.SinusoidalVoltageSource('1', 'x', circuit.gnd,
                                     amplitude=10,
                                     frequency=900)
+ee, nn = 1, 1
+def node() :
+    global nn
+    nn = nn+1
+    return "N" + str(nn)
+
+def el() :
+    global ee
+    ee = ee+1
+    return ee
+
+circuit.include('ada4084.cir')
+circuit.include('ad8606.cir')
 
 EKO()
-def bessel(circuit) :
+def bessel(circuit, a) :
     #https://www.changpuak.ch/electronics/Bessel_Lowpass_active_24dB.php    
-    circuit.include('ada4084.cir')
-    circuit.include('ad8606.cir')
-    R1 = circuit.R(1, 'A',     'n1',       		1@u_kΩ)    
-    R2 = circuit.R(2, 'n1',    'n2',       		1@u_kΩ)    
-    R3 = circuit.R(3, 'n2',     'n4',      		1@u_kΩ)    
-    R4 = circuit.R(4, 'n4',     'n5',      		1@u_kΩ)    
-    C1 = circuit.C(5, 'n1',     'n3',      		14.684@u_nF)    
-    C2 = circuit.C(6, 'n2',     circuit.gnd,      	13.479@u_nF)    
-    C3 = circuit.C(7, 'n4',     'out',      		20.213@u_nF)    
-    C4 = circuit.C(8, 'n5',     circuit.gnd,		7.791@u_nF)    
+    out, n1, n2, n3, n4, n5 = node(), node(), node(), node(), node(), node()
+    R1 = circuit.R(el(), a,     n1,       		1@u_kΩ)    
+    R2 = circuit.R(el(), n1,    n2,       		1@u_kΩ)    
+    R3 = circuit.R(el(), n2,     n4,      		1@u_kΩ)    
+    R4 = circuit.R(el(), n4,     n5,      		1@u_kΩ)    
+    C1 = circuit.C(el(), n1,     n3,      		14.684@u_nF)    
+    C2 = circuit.C(el(), n2,     circuit.gnd,      	13.479@u_nF)    
+    C3 = circuit.C(el(), n4,     out,      		20.213@u_nF)    
+    C4 = circuit.C(el(), n5,     circuit.gnd,		7.791@u_nF)    
     #EKOX(dir(circuit))
-    X1 = circuit.X(551, 'AD8606', 'n2', 'n3', '+5V','-5V', 'n3')
-    X2 = circuit.X(552, 'AD8606', 'n5', 'out', '+5V','-5V', 'out')
-    circuit.V(111, '+5V', circuit.gnd, 'dc 5' )
-    circuit.V(222, '-5V', circuit.gnd, 'dc -5' )
+    X1 = circuit.X(551 + el(), 'AD8606', n2, n3, '+5V','-5V', n3)
+    X2 = circuit.X(552 + el(), 'AD8606', n5, out, '+5V','-5V', out)
+    return out
     
 def RC(circuit) :
-    R1 = circuit.R(2, 'A',     'out',       0.4@u_kΩ)
-    C1 = circuit.C(3, 'out',   circuit.gnd, 0.5@u_uF)
+    KK=0.3
+    R1 = circuit.R(2, 'A',     'out',       KK*0.1@u_kΩ)
+    C1 = circuit.C(3, 'out',   circuit.gnd, 1./KK*0.2@u_uF)
     break_frequency = 1 / (2 * math.pi * float(R1.resistance * C1.capacitance))
     EKOX(break_frequency)
-    circuit.V(111, '+5V', circuit.gnd, 'dc 5' )
-    circuit.V(222, '-5V', circuit.gnd, 'dc -5' )
+    return 'out'
 
 if False :
 
@@ -90,8 +102,16 @@ if False :
     circuit.L('1','N002','N003',0.5)
     circuit.model('Def', 'D')
 
-bessel(circuit)
-#RC(circuit)
+circuit.V(111, '+5V', circuit.gnd, 'dc 5' )
+circuit.V(222, '-5V', circuit.gnd, 'dc -5' )
+
+if False :
+    o1 = bessel(circuit, 'A'); 
+    o2 = bessel(circuit, o1)
+    out = bessel(circuit, o2)
+
+out = RC(circuit)
+
 EKO();
 #print(circuit)
 circuit.PieceWiseLinearVoltageSource('1', 'A', circuit.gnd, values = values)
@@ -104,14 +124,12 @@ EKOX(1./samplerate/K)
 step = (1./samplerate/K)
 analysis = simulator.transient(step_time=step@u_s,
                                end_time=duration@u_s)
-
+EKOT("transient done")
 #EKOX(dir(analysis))
 #EKOX(dir(circuit))
-EKOX(analysis.nodes)
+#EKOX(analysis.nodes)
 EKOX(analysis.elements)
 EKOX(duration / step)
-EKOX(analysis.nodes['out'].shape)
-EKOX(analysis.nodes['a'].shape)
 
 
 def interpolate(s) :
@@ -120,7 +138,7 @@ def interpolate(s) :
     return np.interp(t, tt, s)
 
 
-in_, out = interpolate(np.asarray(analysis.nodes['a'])), interpolate(np.asarray(analysis['out']))
+in_, out = interpolate(np.asarray(analysis.nodes['a'])), interpolate(np.asarray(analysis[out]))
 EKOX(np.var(in_))
 EKOX(np.var(out))
 EKOX(np.var(values[:,1]))
@@ -132,26 +150,46 @@ wavfile.write('speech_in.wav', samplerate, in_)
 EKOX(in_.shape)
 EKOX(out.shape)
 
-EKOX(np.mean(np.abs(in_ - out)))
+sample_width = 1./samplerate
+L = in_.shape[0]
+EKOX(L)
+DD = int(0.020 / sample_width)
+EKOX(DD)
+a = in_[DD:L-DD]
+la = a.shape[0]
+def f(i) : 
+    b = out[i:i+la]
+    return -np.mean(np.abs(a - b))
+aaa = list(map(f, range(DD*2)))
+EKOX(np.argmin(aaa))
+
+EKOX(scipy.__version__)
+EKOX(scipy.signal.find_peaks(aaa))
+delay = np.abs(np.argmin(aaa) - DD) * sample_width
+EKOX(delay)
+
+plt.plot(np.asarray(aaa)); plt.show()
 
 
-current = analysis['a']
-aimax = np.amax(current.data)
-aimin = np.amin(current.data)
-print ('Max Current: ',aimax)
-print ('Min Current: ',aimin)
+if False :
 
-figure1 = plt.figure(1, (20, 10))
-#plt.plot(analysis.time, current, '-')
-plt.plot(analysis.time, analysis['out'], '-')
-plt.grid()
-plt.title('Current')
-plt.xlabel('time')
-plt.ylabel('Amps')
-plt.axhline(y=aimax,color='red')
-plt.axhline(y=aimin,color='red')
-yticks, ytlabels =plt.yticks()
-yticks[-1]=aimax
-yticks[-1]=aimin
-plt.yticks(yticks)
-plt.show()
+    current = analysis['a']
+    aimax = np.amax(current.data)
+    aimin = np.amin(current.data)
+    print ('Max Current: ',aimax)
+    print ('Min Current: ',aimin)
+
+    figure1 = plt.figure(1, (20, 10))
+    #plt.plot(analysis.time, current, '-')
+    plt.plot(analysis.time, analysis['out'], '-')
+    plt.grid()
+    plt.title('Current')
+    plt.xlabel('time')
+    plt.ylabel('Amps')
+    plt.axhline(y=aimax,color='red')
+    plt.axhline(y=aimin,color='red')
+    yticks, ytlabels =plt.yticks()
+    yticks[-1]=aimax
+    yticks[-1]=aimin
+    plt.yticks(yticks)
+    plt.show()
