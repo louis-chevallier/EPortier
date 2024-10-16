89929b48cf15a598680d6d3823f342f187849349
louis chevallier <louis.chevallier@gmail.com>
2024-10-16 10:19:46 +0200

    plot-elec

--- ESP8266/garage/monitor.py
+++ ESP8266/garage/monitor.py
@@ -6,30 +6,45 @@ from utillc import *
 import argparse
 import matplotlib.pyplot as plt
 import numpy as np
-
+from datetime import timedelta
 url = "http://192.168.1.115/data_linky"
-d = {}
+d = {
+    "values" : [],
+    "dates" : {},
+}
+T = 1 # second
+K = 3 #1000
 parser = argparse.ArgumentParser(description='monitor linky')
 parser.add_argument('--read', action='store_true')
+parser.add_argument('--write', action='store_true')
 args = parser.parse_args()
 if args.read :
             with open("data.pickle", "rb") as f:
                 d = pickle.load(f)
-                dates = list(d.keys())
+                dates = list(d["dates"].keys())
                 EKOX(len(dates))
                 dates = sorted(dates)
-                #EKOX('\n'.join(list(map(str, dates[0:100]))))
-                v = [ d[k][2] for k in  dates]
-                plt.plot(v); plt.show()
-else :
+                d0 = dates[0]
+                EKOX(d0)
+                fi = d["dates"][d0]
+                EKOX(fi)                
+                v = np.asarray(d["values"][fi:])
+                v = v[:,1]
+                EKOX(v)
+                n = len(v)
+                EKOX(n)
+                ndates = [ d0 + timedelta(seconds=s * T) for s in range(0,n)]
+                plt.plot(ndates, v); plt.show()
+if args.write :
     for i in range(1000000) :
         with urllib.request.urlopen(url) as p :
             data = json.load(p)
             #print(data["Iinst"])
-            time.sleep(1)
-            now = datetime.datetime.now()
-            d[now] = (data["Iinst"], data["papp"], data["pappm"])
-        if i % 1000 == 0 :
+            time.sleep(T)
+            d["values"].append( (int(float(data["Iinst"]))*100, int(float(data["papp"]))))
+        now = datetime.datetime.now()
+        d["dates"][now] = i
+        if i % K == 0 :
             EKOX(now)
             with open("data.pickle", "wb") as f:
                 pickle.dump(d, f)
