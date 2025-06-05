92aa047b016c0ebf4398ff9f8bd0a81b7fe49c8f
jitdeep chehara <jitdeep.chehara@gmail.com>
2025-06-05 10:22:40 +0200

    mcp3208

--- ESP8266/sound/sound.ino
+++ ESP8266/sound/sound.ino
@@ -11,25 +11,49 @@
 #include "ADS1X15.h"
 #include "websocket.h"
 
+
+
 #include "util.h"
 #include "tasks.h"
 
+
+
+// mcp
+// https://github.com/RobTillaart/MCP_ADC
+#include "MCP_ADC.h"
+
+
+MCP3208 mcp28;
+#define MCP3208_CS_PIN 25
+
+uint32_t start, stop, 
+         analog_read_time,
+         analog_read_multiple_time;
+
+const uint8_t num_channels = 8;
+uint8_t channels_list[num_channels] = {
+    0,1,2,3,4,5,6,7
+};
+
+
+
+
  
 // chez nous
-//const char* ssid = "CHEVALLIER_BORDEAU"; //Enter Wi-Fi SSID
-const char* ssid = "Tenda-2.4G-ext"; //Enter Wi-Fi SSID
+const char* ssid = "CHEVALLIER_BORDEAU"; //Enter Wi-Fi SSID
+//const char* ssid = "Tenda-2.4G-ext"; //Enter Wi-Fi SSID
 const char* password =  "9697abcdea"; //Enter Wi-Fi Password
 
 //const String IPADRESS="176.161.19.7";
 const String WURL = String("http://") + String(IPADDRESS) + ":" + String(__PORT);
 
-
+const int PIN_TOGGLE = 13;
 
 ADS1114 ADS_1(0x49);
 
 long count = 0;
 int ledv = 1>2;
-long start = 0;
+//long start = 0;
 
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
@@ -147,7 +171,6 @@ void setup() {
   server.addHandler(&ws);
   EKO();
 
-
   server.on("/code.js", HTTP_GET, [](AsyncWebServerRequest *request){
       String npage(jscode);
       EKO();
@@ -195,13 +218,179 @@ void setup() {
   file.close();
   EKO();
 
-  
+  pinMode(PIN_TOGGLE, OUTPUT);  
+
+
+  setup_MPC();
   
 }
+
+
 void loop() {
-  auto now = millis();
-  delay(4);    
+
+  // genere un creneau à 1000Hz / 2 sur la broche D7 ( 13)
+  auto now = micros();
+  delayMicroseconds(4);    
   if (now > last + 1000) {
     last = now;
+    digitalWrite(PIN_TOGGLE, !digitalRead(PIN_TOGGLE));
   }
+}
+
+
+
+
+void setup_MPC()
+{
+
+  Serial.println(__FILE__);
+  Serial.print("MCP_ADC_LIB_VERSION: ");
+  Serial.println(MCP_ADC_LIB_VERSION);
+
+  SPI.begin();
+
+  Serial.println();
+  Serial.println("ADC\tCHAN\tMAXVALUE");
+  Serial.print("mcp28\t");
+  Serial.print(mcp28.channels());
+  Serial.print("\t");
+  Serial.println(mcp28.maxValue());
+
+  Serial.println("\nTiming in micros().\n");
+  delay(10);
+
+  Serial.println("***************************************\n");
+  for (int s = 1; s <= 16; s++)
+  {
+    Serial.println(s * 1000000UL);
+    mcp28.setSPIspeed(s * 1000000UL);
+    mcp28.begin(MCP3208_CS_PIN);
+    test();
+  }
+
+  testChannelsRead();
+  Serial.println("done...");
+}
+
+
+
+void test()
+{
+  uint32_t val = 0;
+
+  start = micros();
+  for (int channel = 0; channel < mcp28.channels(); channel++)
+  {
+    val += mcp28.read(channel);
+  }
+  stop = micros();
+  analog_read_time = stop - start;
+  
+  Serial.print("mcp28.read()\t8x: \t");
+  Serial.println(analog_read_time);
+  delay(10);
+
+
+  start = micros();
+  int16_t readings[num_channels];
+  
+  mcp28.readMultiple(channels_list, num_channels, readings);
+  stop = micros();
+  analog_read_multiple_time = stop - start;
+
+  Serial.print("mcp28.readMultiple()\t8x: \t");
+  Serial.println(stop - start);
+
+  Serial.print("read() time / readMultiple() time \t");
+  Serial.println((1.0 * analog_read_time) / analog_read_multiple_time);
+  delay(10);
+
+
+  start = micros();
+  for (int channel = 0; channel < mcp28.channels(); channel++)
+  {
+    val += mcp28.differentialRead(channel);
+  }
+  stop = micros();
+  Serial.print("mcp28.differentialRead() 8x: \t");
+  Serial.println(stop - start);
+  delay(10);
+
+  start = micros();
+  for (int channel = 0; channel < mcp28.channels(); channel++)
+  {
+    val += mcp28.deltaRead(channel);
+  }
+  stop = micros();
+  Serial.print("mcp28.deltaRead()\t8x: \t");
+  Serial.println(stop - start);
+  Serial.println();
+  delay(10);
+
+}
+
+
+void testChannelsRead() {
+  Serial.println("***************************************\n");
+
+  mcp28.setSPIspeed(8000000);  //  8 MHz
+  mcp28.begin(MCP3208_CS_PIN);
+  Serial.println("8000000");
+
+  for (uint8_t numChannelsToRead = 1; numChannelsToRead <= num_channels; numChannelsToRead++) {
+
+    delay(10);
+
+    //  read()
+    start = micros();
+    for (uint8_t i = 0; i < numChannelsToRead; i++) {
+      mcp28.read(i);
+    }
+    stop = micros();
+    analog_read_time = stop - start;
+
+    Serial.print("mcp28.read()\t");
+    Serial.print(numChannelsToRead);
+    Serial.print(": \t");
+    Serial.print(analog_read_time);
+    Serial.println("us");
+
+    //  readMultiple()
+    uint8_t channels_list[numChannelsToRead];
+    for (uint8_t i = 0; i < numChannelsToRead; i++) {
+      channels_list[i] = i;
+    }
+
+    delay(10);
+
+    int16_t readings[numChannelsToRead];
+    start = micros();
+    mcp28.readMultiple(channels_list, numChannelsToRead, readings);
+    stop = micros();
+    analog_read_multiple_time = stop - start;
+
+    Serial.print("mcp28.readMultiple()\t");
+    Serial.print(numChannelsToRead);
+    Serial.print(": \t");
+    Serial.print(analog_read_multiple_time);
+    Serial.println("us");
+    Serial.print("read() time / readMultiple() time \t");
+    Serial.println((1.0 * analog_read_time) / analog_read_multiple_time, 2);  //  print as float
+    
+    Serial.println("\n");
+    delay(10);
+
+
+    
+
+
+
+
+
+
+
+
+    
+  }
+
 }
